47d30cf89a5651bb3b55719f104137aec75d33c6
Andrew Nelson <andy@andyhub.com>
2017-12-29 12:46:37 -0500

    finished fitbit part

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var request = require('request')
 // Data vars
 var steps = 0
 var calories = 0
+var distance = 0
 
 // Time vars
 var millisInADay = 86400000
@@ -29,6 +30,10 @@ var fbClient = new FitbitApiClient(process.env.FB_CLIENT_ID, process.env.FB_CLIE
 // initialize the Google API client
 var google = require('googleapis')
 var gFit = google.fitness('v1')
+var scopes = [
+  'https://www.googleapis.com/auth/fitness.activity.read',
+  'https://www.googleapis.com/auth/fitness.location.read'
+]
 var gUrl = 'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate'
 var GAuth = google.auth.OAuth2
 var gClient = new GAuth(
@@ -40,7 +45,7 @@ var gClient = new GAuth(
 // redirect the user to the Fitbit authorization page
 app.get('/gAuthorize', function (req, res) {
   // request access to the user's activity scope
-  res.redirect(gClient.generateAuthUrl({scope: 'https://www.googleapis.com/auth/fitness.activity.read'}))
+  res.redirect(gClient.generateAuthUrl({scope: scopes}))
 })
 
 app.get('/gCallback', function (req, res) {
@@ -55,7 +60,6 @@ app.get('/gCallback', function (req, res) {
     }
     var stepsBody = {
       aggregateBy: [{
-        dataSourceId: 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps',
         dataTypeName: 'com.google.step_count.delta'
       }],
       bucketByTime: {
@@ -69,7 +73,7 @@ app.get('/gCallback', function (req, res) {
         return console.error('steps failed:', err);
       }
       steps = body.bucket[0].dataset[0].point[0].value[0].intVal
-      console.log(steps)
+      console.log('steps: ', steps)
 
       var caloriesBody = {
         aggregateBy: [{
@@ -94,29 +98,52 @@ app.get('/gCallback', function (req, res) {
             calories += buckets[i].dataset[0].point[0].value[0].fpVal
           }
         }
-        console.log(calories)
-        res.redirect(fbClient.getAuthorizeUrl('activity', process.env.FB_CALLBACK_URL))
+        console.log('calories: ', calories)
+
+        var distanceBody = {
+          aggregateBy: [{
+            dataTypeName: 'com.google.distance.delta'
+          }],
+          bucketByActivityType: {
+            minDurationMillis: 0
+          },
+          startTimeMillis: startTimeMillis,
+          endTimeMillis: endTimeMillis
+        }
+
+        request.post({url: gUrl, auth: auth, json: distanceBody}, function (err, httpResponse, body) {
+          if (err) {
+            return console.error('distance failed:', err);
+          }
+          var buckets = body.bucket
+          for (var i = 0; i < buckets.length; ++i) {
+            var activity = buckets[i].activity
+            if (activity !== 3) { // https://developers.google.com/fit/rest/v1/reference/activity-types
+              distance += buckets[i].dataset[0].point[0].value[0].fpVal
+            }
+          }
+          distance *= 0.000621371192 //convert to miles
+          console.log('distance: ', distance)
+
+          res.redirect(fbClient.getAuthorizeUrl('activity', process.env.FB_CALLBACK_URL))
+        })
       })
     })
   })
 })
 
-// redirect the user to the Fitbit authorization page
-// app.get('/fbAuthorize', function (req, res) {
-//   // request access to the user's activity scope
-//   res.redirect(fbClient.getAuthorizeUrl('activity', process.env.FB_CALLBACK_URL))
-// })
-
 // handle the callback from the Fitbit authorization flow
 app.get('/fbCallback', function (req, res) {
   // exchange the authorization code we just received for an access token
-  fbClient.getAccessToken(req.query.code, process.env.CALLBACK_URL).then(function (result) {
+  fbClient.getAccessToken(req.query.code, process.env.FB_CALLBACK_URL).then(function (result) {
     var stepsBody = {
-      activityId: 90013, // https://dev.fitbit.com/reference/web-api/explore/#/Activity/activity6
+      activityId: 17190, // https://dev.fitbit.com/reference/web-api/explore/#/Activity/activity6
       date: startTimeString,
       startTime: '00:00:00',
       durationMillis: millisInADay,
-      steps: steps
+      steps: steps,
+      distance: distance,
+      manualCalories: parseInt(calories)
     }
     // var nameBody = {
     //   activityName: 'Google Fit Activity', // https://dev.fitbit.com/reference/web-api/activity
@@ -127,7 +154,6 @@ app.get('/fbCallback', function (req, res) {
     // }
     fbClient.post('/activities.json', result.access_token, stepsBody).then(function (results) {
       res.send(results[0])
-      console.log(results[0])
     })
   }).catch(function (error) {
     res.send(error)
